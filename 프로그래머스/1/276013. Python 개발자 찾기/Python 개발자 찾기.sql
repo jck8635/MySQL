bb4a36f12e078d1c3347dee689e515bba2965ce2
jck8635 <jck8635@gmail.com>
2026-08-23 14:53:56 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -0,0 +1,6 @@
+-- 코드를 작성해주세요
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPER_INFOS
+WHERE SKILL_1 = 'Python' or SKILL_2 = 'Python' or SKILL_3 = 'Python'
+/* WHERE 'Python' in (SKILL_1, SKILL_2, SKILL_3) */
+ORDER BY ID;
